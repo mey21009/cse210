0bc4fbb6f0c1bbc295dc484ba6482a505221503c
mey21009 <mey2100@byui.edu>
2023-05-13 23:46:39 -0500

    wk4

--- prove/Develop04/BreathingActivity.cs
+++ prove/Develop04/BreathingActivity.cs
@@ -2,13 +2,12 @@ using System;
 
 public class BreathingActivity : Activity
 {
-    // Attributes 
+   
     private string _message1 = "Breathe in...";
-    private string _message2 = "Now breathe out...";
-    private string _description = "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.";
+    private string _message2 = "Breathe out...";
+    private string _description = "This activity will help you relax your breathing by breathing in and out slowly. Focus on breathing.";
 
-    // Constructors
-    // Methods
+   
     public BreathingActivity(string activityName, int activityTime) : base(activityName, activityTime)
     {
 
@@ -20,29 +19,29 @@ public class BreathingActivity : Activity
 
     public void Breathing(int seconds)
     {
-        Console.WriteLine();  //insert blank line to start
+        Console.WriteLine(); 
         int secondsTimer = 0;
         while (secondsTimer < seconds)
         {
-            Console.WriteLine();  //insert blank line to start
+            Console.WriteLine(); 
             for (int i = 4; i > 0; i--)
             {
                 Console.Write($"{_message1}{i}");
                 Thread.Sleep(1000);
-                string blank = new string('\b', (_message1.Length + 2));  // Overwrite line
+                string blank = new string('\b', (_message1.Length + 2));  
                 Console.Write(blank);
                 secondsTimer += 1;
             }
-            Console.WriteLine($"{_message1}  ");  // last prompt
+            Console.WriteLine($"{_message1}  ");  
             for (int i = 5; i > 0; i--)
             {
                 Console.Write($"{_message2}{i}");
                 Thread.Sleep(1000);
-                string blank = new string('\b', (_message2.Length + 2));  // Overwrite line
+                string blank = new string('\b', (_message2.Length + 2));  
                 Console.Write(blank);
                 secondsTimer += 1;
             }
-            Console.WriteLine($"{_message2}  ");  // last prompt
+            Console.WriteLine($"{_message2}  ");  
         }
     }
 }
