cf32e6ce258666de8e19c35514d47a7bc72b8f12
mey21009 <mey2100@byui.edu>
2023-05-27 22:54:23 -0500

    week 7

--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -5,14 +5,14 @@ class Program
     static void Main(string[] args)
     {
         List<Activity> activities =  new List<Activity>();
-        activities.Add(new RunningActivity("12 Jan 2021",6.7,30));
-        activities.Add(new CyclingActivity("22 Feb 2015",9.6,30));
-        activities.Add(new Swimming("27 Nov 2022",96,30));
+        activities.Add(new RunningActivity("12 Feb 2024",8.7,25));
+        activities.Add(new CyclingActivity("13 Jan 2020",10.6,25));
+        activities.Add(new Swimming("20 Sept 2023",90,25));
 
         int index = 1;
         foreach(Activity activity in activities)
         {
-            Console.WriteLine($"\nACTIVITY N°{index}:\n{activity.GetSummary()}");
+            Console.WriteLine($"\nACTIVITY No{index}:\n{activity.GetSummary()}");
             index++;
         }
         Console.WriteLine();    
